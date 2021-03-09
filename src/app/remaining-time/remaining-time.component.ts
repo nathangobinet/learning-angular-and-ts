c1d4442fe2f3347aad34cc154063e24db9f69569
Nathan Gobinet <nathan.gobinet@gmail.com>
2021-03-09 19:22:14 +0100

    Fix remaining time display

--- src/app/remaining-time/remaining-time.component.ts
+++ src/app/remaining-time/remaining-time.component.ts
@@ -8,8 +8,18 @@ import { TimeService } from '../time.service';
 })
 export class RemainingTimeComponent{
   timeService : TimeService;
-  
+
   constructor(timeService : TimeService) { 
     this.timeService = timeService;
+  }
+
+  getMinute() : string {
+    const minutes = Math.floor(this.timeService.remainingSeconds / 60);
+    return (`0${minutes}`).slice(-2);
+  }
+
+  getSecond() : string {
+    const seconds = this.timeService.remainingSeconds % 60;
+    return (`0${seconds}`).slice(-2);
   }
 }
